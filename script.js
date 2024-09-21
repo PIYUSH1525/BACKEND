680f3032097b21ed59347a8091af7f5775971183
PIYUSH1525 <negipiyush125@gmail.com>
2024-09-21 22:00:36 +0530

    BACKEND JS

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// alert("Hello Running"); //pops up messsage on opening of new window
-// // var arr =[1, 2,2, 3,4 ]
-// var one = "happy"
-// var two = "marne"
-// console.log(one,"and" ,two )
+// // alert("Hello Running"); //pops up messsage on opening of new window
+// // // var arr =[1, 2,2, 3,4 ]
+// // var one = "happy"
+// // var two = "marne"
+// // console.log(one,"and" ,two )
 
 
+// console.log(a,b);  //hoisting implememnts here 
+// var a = 10;
+// var b 
+
+
+a = 10;
+b = a;
+console.log(b);                         //Primitive data types
+b = b-5;
 console.log(a,b);
-var a = 10;
-var b 
