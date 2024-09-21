4a3ec8fd56a09555fc677cccd809ed6554504950
PIYUSH1525 <negipiyush125@gmail.com>
2024-09-21 22:27:42 +0530

    BACKEND JS

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-// // // // alert("Hello Running"); //pops up messsage on opening of new window
-// // // // // var arr =[1, 2,2, 3,4 ]
-// // // // var one = "happy"
-// // // // var two = "marne"
-// // // // console.log(one,"and" ,two )
+// // // // // alert("Hello Running"); //pops up messsage on opening of new window
+// // // // // // var arr =[1, 2,2, 3,4 ]
+// // // // // var one = "happy"
+// // // // // var two = "marne"
+// // // // // console.log(one,"and" ,two )
 
 
-// // // console.log(a,b);  //hoisting implememnts here 
-// // // var a = 10;
-// // // var b 
+// // // // console.log(a,b);  //hoisting implememnts here 
+// // // // var a = 10;
+// // // // var b 
 
 
-// // // a = 10;
-// // // b = a;
-// // // console.log(b);                         //Primitive data types
-// // // b = b-5;
-// // // console.log(a,b);
+// // // // a = 10;
+// // // // b = a;
+// // // // console.log(b);                         //Primitive data types
+// // // // b = b-5;
+// // // // console.log(a,b);
 
 
-// // if ("mango"){
-// //     console.log("apple");               //conditonal
+// // // if ("mango"){
+// // //     console.log("apple");               //conditonal
+    
+// // // }
+
+// // // for(start:end:change)
+// // for(var i = 0 ; i<11; i++){
+// //     console.log(i);                             //for loop
     
 // // }
 
-// // for(start:end:change)
-// for(var i = 0 ; i<11; i++){
-//     console.log(i);                             //for loop
-    
+
+// i= 10
+// while(i>5){
+//     console.log(i);
+//     i-=1  
 // }
 
 
-i= 10
-while(i>5){
-    console.log(i);
-    i-=1
+function function_name(){
 
-    
 }
