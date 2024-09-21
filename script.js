cc5d3c6dfa899d45fffdedb03ba5c7eb600810fc
PIYUSH1525 <negipiyush125@gmail.com>
2024-09-21 21:46:07 +0530

    BACKEND JS

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hello Running")  //pops up messsage on opening of new window gh
+// alert("Hello Running"); //pops up messsage on opening of new window
+// // var arr =[1, 2,2, 3,4 ]
+// var one = "happy"
+// var two = "marne"
+// console.log(one,"and" ,two )
+
+
+console.log(a,b);
+var a = 10;
+var b 
