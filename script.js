70007dfb6471aaaba2c12c512e6e9acfde269603
PIYUSH1525 <negipiyush125@gmail.com>
2024-09-22 23:50:31 +0530

    BACKEND JS

--- script.js
+++ script.js
@@ -1,83 +1,83 @@
-// alert("Hello Running"); //pops up messsage on opening of new window
-// // var arr =[1, 2,2, 3,4 ]
-  // var one = "happy"
-  // var two = "marne"
-  // console.log(o
-//  // console.log(a,b);  //hoisting implememnts here 
-//  // var a = 10;
-//  // var b 
+// // alert("Hello Running"); //pops up messsage on opening of new window
+// // // var arr =[1, 2,2, 3,4 ]
+//   // var one = "happy"
+//   // var two = "marne"
+//   // console.log(o
+// //  // console.log(a,b);  //hoisting implememnts here 
+// //  // var a = 10;
+// //  // var b 
 
 
- // a = 10;
- // b = a;
- // console.log(b);                         //Primitive data types
- // b = b-5;
- // console.log(a,b);
+//  // a = 10;
+//  // b = a;
+//  // console.log(b);                         //Primitive data types
+//  // b = b-5;
+//  // console.log(a,b);
 
 
- // if ("mango"){
- //     console.log("apple");               //conditonal
+//  // if ("mango"){
+//  //     console.log("apple");               //conditonal
     
- // }
- // for(start:end:change)
-// // for(var i = 0 ; i<11; i++){
- // //     console.log(i);                             //for loop
+//  // }
+//  // for(start:end:change)
+// // // for(var i = 0 ; i<11; i++){
+//  // //     console.log(i);                             //for loop
     
- // }
+//  // }
 
 
-// i= 10
-//  while(i>5){
-//      console.log(i);
-//     i-=1  
-//}
+// // i= 10
+// //  while(i>5){
+// //      console.log(i);
+// //     i-=1  
+// //}
 
 
- // function function_name(){
+//  // function function_name(){
 
- // }
+//  // }
 
- // function sayhi(){
- //     console.log("sayhello");
- // }
- // sayhi();
-// // // function summ(x,y){
-// // //     console.log(x+y);
-// // // }
-// // // summ(3,4);
+//  // function sayhi(){
+//  //     console.log("sayhello");
+//  // }
+//  // sayhi();
+// // // // function summ(x,y){
+// // // //     console.log(x+y);
+// // // // }
+// // // // summ(3,4);
 
-//  var c = [1,4,"hello", 2,5,7]
-// c.splice(2,2)
-// //  console.log(c[2]);
-// //  c.push(34, 55)
-//  console.log(c);
-// //  c.pop()
+// //  var c = [1,4,"hello", 2,5,7]
+// // c.splice(2,2)
+// // //  console.log(c[2]);
+// // //  c.push(34, 55)
 // //  console.log(c);
-// // c.unshift("satrt")
-// //  console.log(c);
-// //  c.shift();
-// //  console.log(c);
+// // //  c.pop()
+// // //  console.log(c);
+// // // c.unshift("satrt")
+// // //  console.log(c);
+// // //  c.shift();
+// // //  console.log(c);
  
-//  var Piyush = {
-//     Name : "Piyush Negi",
-//     age : 22,
-//     height: 5.11,
-//     weight:60
-//  }
+// //  var Piyush = {
+// //     Name : "Piyush Negi",
+// //     age : 22,
+// //     height: 5.11,
+// //     weight:60
+// //  }
 
-//  console.log(Piyush.weight);
-//  Piyush.height = 5.10
-//  console.log(Piyush.height);
+// //  console.log(Piyush.weight);
+// //  Piyush.height = 5.10
+// //  console.log(Piyush.height);
  
-//   var array = [1,3,5,78,3,69]
-//   array.forEach(function(all){
-//     console.log(all+"Piyush");
-//     console.log(all+" Piyush");
-//   })
+// //   var array = [1,3,5,78,3,69]
+// //   array.forEach(function(all){
+// //     console.log(all+"Piyush");
+// //     console.log(all+" Piyush");
+// //   })
 
-var alpha = [1,5,6,3,0]
-var newarr = alpha.map(function(value){
-    return value + 10
-})
-console.log(alpha);
-console.log(newarr);
+// var alpha = [1,5,6,3,0]
+// var newarr = alpha.map(function(value){
+//     return value + 10 
+// })
+// console.log(alpha);
+// console.log(newarr);
