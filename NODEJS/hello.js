5d69f7cd9b3e3c3d5c1adb74ff54c323bb2190db
PIYUSH1525 <negipiyush125@gmail.com>
2024-10-19 22:48:01 +0530

    Merge

--- NODEJS/hello.js
+++ NODEJS/hello.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const fs = require('node:fs')
+// fs.writeFile("piyush.txt" , "Hello this is a new file " ,function(err){
+//     if (err)
+//         console.error("error");
+//     else
+//         console.log("Done"); 
+// })
+fs.appendFile("piyush.txt"," the starting of nodejs" , function(err){
+    if (err)
+        console.error("Problem in the code")
+    else
+        console.log("Done")
+})
