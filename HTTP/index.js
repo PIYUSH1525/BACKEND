797990a3245d9a9f7aa54e31afbc79719e96fbc6
PIYUSH1525 <negipiyush125@gmail.com>
2024-10-21 00:03:36 +0530

    Merge

---
+++ HTTP/index.js
@@ -0,0 +1,11 @@
+const http = require('node:http');
+
+const MyServer = http.createServer(function(req,res){
+    console.log("request recived");
+    res.end("DONE HERE")
+})
+
+MyServer.listen(2000,function(){
+    console.log("Server runninng");
+    
+})
