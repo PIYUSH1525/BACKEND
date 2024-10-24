d78d0338bf3347dd3740d0792613333314be7fcb
PIYUSH1525 <negipiyush125@gmail.com>
2024-10-24 21:53:31 +0530

    Express

--- ExpressJS/script.js
+++ ExpressJS/script.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(function(req,res,next){
+    console.log("Middle ware")
+    next();
+})
+app.use(function(req,res,next){
+    console.log("One mor eMiddle ware")
+    next();
+})
+
 app.get("/" , function(req,res){
     res.send("yea mera page ha toh");
 })
