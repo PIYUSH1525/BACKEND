3bae655f2d953f0b836c8b8f5bff19e95f671efe
PIYUSH1525 <negipiyush125@gmail.com>
2024-10-28 23:02:50 +0530

    Express

--- ExpressJS/script.js
+++ ExpressJS/script.js
@@ -14,8 +14,14 @@ app.use(function(req,res,next){
     next();
 })
 
-app.get("/bhai" , function(req,res){
-    res.send("yea mera bhai ka page ha");
+app.get("/bhai" , function(req,res,next){
+    // res.send("yea mera bhai ka page ha");
+    return next(new Error("Something Wrong"))
+})
+
+app.use(function(err,req,res,next){
+    console.error(err)
+    res.status(500).send("ERoOR In SERVER")
 })
 
 app.listen(3000,function(){
