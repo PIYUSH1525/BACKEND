036cde2e8d6037016738ccbdaf0c01856c7b1f3c
PIYUSH1525 <negipiyush125@gmail.com>
2024-11-02 19:06:03 +0530

    Express

--- BACK_PROJECT/index.js
+++ BACK_PROJECT/index.js
@@ -1,6 +1,14 @@
-const express = require('node:express')
-const app = express
+const express = require('express')
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+
+app.get("/" ,function(req,res){
+    res.send("WORKING")
+})
+
+app.listen(3000 ,function(){
+    console.log("Console ruuning");
+})
